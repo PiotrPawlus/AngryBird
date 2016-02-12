1c2cca6d40673312e7eb72b031f1cc6842bdc4eb
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-12 09:14:13 +0100

    add zPosition for objects

---
+++ AngryBird/ObjectZPosition.swift
@@ -0,0 +1,16 @@
+//
+//  ObjectZPosition.swift
+//  AngryBird
+//
+//  Created by Piotr Pawluś on 12/02/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import UIKit
+
+struct ObjectZPosition {
+    static let hud: CGFloat = 3.0
+    static let foreground: CGFloat = 2.0
+    static let middleground: CGFloat = 1.0
+    static let background: CGFloat = -2.0
+}
