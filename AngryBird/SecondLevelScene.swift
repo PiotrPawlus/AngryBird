e60361277d52bcdde7fd1f4d414bfc38e9ba8ff5
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-14 17:42:32 +0100

    #1 Add second level view

--- AngryBird/SecondLevelScene.swift
+++ AngryBird/SecondLevelScene.swift
@@ -9,8 +9,9 @@
 import SpriteKit
 
 class SecondLevelScene: SKScene, SKPhysicsContactDelegate {
-    var menuButton: SKButtonNode!
     var pointLabel: SKLabelNode!
+    var shared: SharedNode!
+    var firstPig: PigSpriteNode!
     
     override init(size: CGSize) {
         super.init(size: size)
@@ -18,14 +19,68 @@ class SecondLevelScene: SKScene, SKPhysicsContactDelegate {
     
         self.physicsWorld.contactDelegate = self
         
-        let shared = SharedNode(size: size, scene: self)
+        shared = SharedNode(size: size, scene: self)
         self.addChild(shared)
+        
+
+        self.addChild(shared.pointLabel)
+        
+        let firstStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: self.frame.size.width - 50.0, posY: shared.ground.size.height - 5.0, rotateOnDegree: 0.0)
+        self.addChild(firstStone)
+        
+        let secondStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: self.frame.size.width - 125.0, posY: shared.ground.size.height - 5.0, rotateOnDegree: 0.0)
+        self.addChild(secondStone)
+        
+        let thirdStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: self.frame.size.width - 87.0, posY: shared.ground.size.height + 49.0, rotateOnDegree: 90.0)
+        self.addChild(thirdStone)
+        
+        let fourthStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: self.frame.size.width - 50.0, posY: shared.ground.size.height + firstStone.size.height + thirdStone.size.width - 5.0, rotateOnDegree: 0.0)
+        self.addChild(fourthStone)
+        
+        let fifthStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: self.frame.size.width - 125.0, posY: shared.ground.size.height + secondStone.size.height + thirdStone.size.width - 5.0, rotateOnDegree: 0.0)
+        self.addChild(fifthStone)
+        
+        let sixthStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: self.frame.size.width - 87.0, posY: shared.ground.size.height + firstStone.size.height + thirdStone.size.width + 50.0, rotateOnDegree: 90.0)
+        self.addChild(sixthStone)
+        
+        firstPig = PigSpriteNode(imageNamed: "wrog", size: size, posX: self.frame.size.width - 85.0 , posY: sixthStone.position.y - 60.0)
+        self.addChild(firstPig)
+        
         
         Level.gameLevel = 2
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        shared.pointLabel.text = "Points: \(PointsCounter.points)"
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask == CollisionCategoryBitmask.Stone) {
+            (contact.bodyA.node as! StoneSpirteNode).takeHP()
+        }
+        if (contact.bodyB.categoryBitMask == CollisionCategoryBitmask.Stone) {
+            (contact.bodyB.node as! StoneSpirteNode).takeHP()
+            
+        }
+        if (contact.bodyA.categoryBitMask == CollisionCategoryBitmask.Pig) {
+            if (contact.bodyA.node as! PigSpriteNode).destroyPig() {
+                // udostępnij poziom
+                self.view?.presentScene(FinishedLevelScene(size: self.size), transition: SKTransition.fadeWithDuration(0.5))
+                Level.unlockLevel(Level.gameLevel)
+            }
+        }
+        if (contact.bodyB.categoryBitMask == CollisionCategoryBitmask.Pig) {
+            if (contact.bodyB.node as! PigSpriteNode).destroyPig() {
+                // udostępnij poziom
+                
+                self.view?.presentScene(FinishedLevelScene(size: self.size), transition: SKTransition.fadeWithDuration(0.5))
+                Level.unlockLevel(Level.gameLevel)
+            }
+        }
     }
 
 }
