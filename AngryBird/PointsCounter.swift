1cedca7c5ed2a1ef99219ce671303a7d8002efd8
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-15 11:35:02 +0100

    #1 saving high score for levels

--- AngryBird/PointsCounter.swift
+++ AngryBird/PointsCounter.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class PointsCounter {
+struct PointsCounter {
     static var points: Int = 0
+    static var highScoreFirstLvl: Int = 0
+    static var highScoreSecondLvl: Int = 0
+    static var highScoreThirdLvl: Int = 0
     static var enableCounting = false
     static var pigTouched = false
+
     
-    class func addPoint() -> Bool {
+    static func downloadScores() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+
+        print("TRZECI PRZED: \(PointsCounter.highScoreThirdLvl)")
+        
+        PointsCounter.highScoreFirstLvl = defaults.integerForKey("highScoreFirstLvl")
+        PointsCounter.highScoreSecondLvl = defaults.integerForKey("highScoreSecondLvl")
+        PointsCounter.highScoreThirdLvl = defaults.integerForKey("highScoreThirdLvl")
+
+        print("TRZECI: \(PointsCounter.highScoreThirdLvl)")
+    }
+    
+    static func addPoint() -> Bool {
         if enableCounting {
             points += 1
         }
@@ -23,8 +39,48 @@ class PointsCounter {
         return enableCounting
     }
     
-    class func resetPoints() {
+    static func resetPoints() {
         PointsCounter.enableCounting = false
         PointsCounter.points = 0
+    }
+    
+    static func saveHighScore(forLevel level: Int) {
+        var high = 0
+        var key = "highScoreFirstLvl"
+        switch level {
+        case 1:
+            PointsCounter.highScoreFirstLvl = max(PointsCounter.points, PointsCounter.highScoreFirstLvl)
+            high = PointsCounter.highScoreFirstLvl
+            key = "highScoreFirstLvl"
+        case 2:
+            PointsCounter.highScoreSecondLvl = max(PointsCounter.points, PointsCounter.highScoreSecondLvl)
+            high = PointsCounter.highScoreSecondLvl
+            key = "highScoreSecondLvl"
+        case 3:
+            PointsCounter.highScoreThirdLvl = max(PointsCounter.points, PointsCounter.highScoreThirdLvl)
+            high = PointsCounter.highScoreThirdLvl
+            key = "highScoreThirdLvl"
+        default:
+            PointsCounter.highScoreFirstLvl = max(PointsCounter.points, PointsCounter.highScoreFirstLvl)
+            high = PointsCounter.highScoreFirstLvl
+        }
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(high, forKey: key)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    static func getHighScore(forLevel level: Int) -> Int{
+        var tempLevel = PointsCounter.highScoreFirstLvl
+        switch level {
+        case 1:
+            tempLevel = PointsCounter.highScoreFirstLvl
+        case 2:
+            tempLevel = PointsCounter.highScoreSecondLvl
+        case 3:
+            tempLevel = PointsCounter.highScoreThirdLvl
+        default:
+            tempLevel = 666
+        }
+        return tempLevel
     }
 }
