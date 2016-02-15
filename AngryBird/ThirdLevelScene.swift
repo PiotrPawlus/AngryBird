3b346737a7d38925c45bd60e888b91a35f0a037e
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-15 16:42:10 +0100

    #1 create third lvl view

--- AngryBird/ThirdLevelScene.swift
+++ AngryBird/ThirdLevelScene.swift
@@ -10,8 +10,11 @@ import SpriteKit
 
 class ThirdLevelScene: SKScene, SKPhysicsContactDelegate {
     var firstPig: PigSpriteNode!
+    var secondPig: PigSpriteNode!
+
     var shared: SharedNode!
     weak var deg: MenuScene?
+    var countOfPigs = 2
     
     override init(size: CGSize) {
         super.init(size: size)
@@ -21,19 +24,40 @@ class ThirdLevelScene: SKScene, SKPhysicsContactDelegate {
         shared = SharedNode(size: size, scene: self)
         self.addChild(shared)
         
-        // Set ground
         
         self.addChild(shared.pointLabel)
-        let firstStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: self.frame.size.width - 50.0, posY: shared.ground.size.height - 1.0, rotateOnDegree: 0.0)
+        
+        let groundSize = shared.ground.size.height
+        
+        let firstStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: self.frame.size.width / 2 + 50.0, posY: groundSize, rotateOnDegree: 0.0)
         self.addChild(firstStone)
         
-        let secondStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: self.frame.size.width - 125.0, posY: shared.ground.size.height - 1.0, rotateOnDegree: 0.0)
+        let posYAboveFirst = groundSize + firstStone.size.height - 10.0
+        
+        let secondStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: firstStone.position.x - firstStone.size.height + 30.0, posY: groundSize - 30.0
+            , rotateOnDegree: 90.0)
         self.addChild(secondStone)
         
-        let thirdStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: self.frame.size.width - 87.0, posY: firstStone.size.height + 55.0, rotateOnDegree: 90.0)
+        let nextToFirstPosX = firstStone.position.x + firstStone.size.width
+      
+
+        let thirdStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: nextToFirstPosX + firstStone.size.width, posY: posYAboveFirst - 20.0, rotateOnDegree: 90.0)
         self.addChild(thirdStone)
         
-        firstPig = PigSpriteNode(imageNamed: "wrog", size: size, posX: self.frame.size.width - 85.0 , posY: shared.ground.size.height - 1.0)
+        let fourthStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: nextToFirstPosX + thirdStone.size.height - 10.0, posY: groundSize, rotateOnDegree: 0.0)
+        self.addChild(fourthStone)
+        
+        let fifthStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: nextToFirstPosX, posY: thirdStone.position.y + firstStone.size.width + 30.0, rotateOnDegree: 0.0)
+        self.addChild(fifthStone)
+        
+        let sixthStone = StoneSpirteNode(imageNamed: "stone_v_2", size: size, posX: nextToFirstPosX + fifthStone.size.width + 20.0, posY: thirdStone.position.y + firstStone.size.width + 30.0, rotateOnDegree: 0.0)
+        self.addChild(sixthStone)
+        
+        let seventhStone = StoneSpirteNode(imageNamed: "stone_v_1", size: size, posX: fourthStone.position.x + fourthStone.size.width + 30.0, posY: groundSize - 20.0, rotateOnDegree: 270.0)
+        self.addChild(seventhStone)
+        
+
+        firstPig = PigSpriteNode(imageNamed: "wrog", size: size, posX: firstStone.position.x + firstStone.size.height / 2 , posY: groundSize)
         self.addChild(firstPig)
         
         Level.gameLevel = 3
