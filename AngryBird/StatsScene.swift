c0ba5a569d862e5c250353b4d1da016ace4dcbdf
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-16 15:36:24 +0100

    change y position for labels and button in stats

--- AngryBird/StatsScene.swift
+++ AngryBird/StatsScene.swift
@@ -14,19 +14,24 @@ class StatsScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
         
+        
+        let background = BackgroundSpriteNode(imageNamed: "background", size: size)
+        background.zPosition = ObjectZPosition.background
+        self.addChild(background)
+        
 
         let menuButton = SKButtonNode(defaultButtonImage: "menu", activeButtonImage: "menu", disabledButtonImage: "menu", buttonAction: self.backToMenu)
         menuButton.enabled = true
-        menuButton.position = CGPointMake(self.frame.width / 2 , 50.0)
+        menuButton.position = CGPointMake(self.frame.midX , self.frame.maxY * 3/10)
         self.addChild(menuButton)
         
-        let first = displayHighScoreForLevel(1, y: 130.0)
+        let first = displayHighScoreForLevel(1, y: self.frame.maxY * 3/9)
         self.addChild(first)
         
-        let second = displayHighScoreForLevel(2, y: 160.0)
+        let second = displayHighScoreForLevel(2, y: self.frame.maxY * 4/9)
         self.addChild(second)
         
-        let third = displayHighScoreForLevel(3, y: 190.0)
+        let third = displayHighScoreForLevel(3, y: self.frame.maxY * 5/9)
         self.addChild(third)
         
         let level = dipslayMaxLevel()
@@ -41,8 +46,8 @@ class StatsScene: SKScene {
         let highScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
         highScoreLabel.fontColor = UIColor.redColor()
         highScoreLabel.fontSize = 25
-        highScoreLabel.position = CGPoint(x: self.frame.width / 2, y: self.frame.height - y)
-        highScoreLabel.text = "HighScore \(level): \(PointsCounter.getHighScore(forLevel: level))"
+        highScoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.height - y)
+        highScoreLabel.text = "High Score for lvl \(level): \(PointsCounter.getHighScore(forLevel: level))"
         return highScoreLabel
     }
     
@@ -50,7 +55,7 @@ class StatsScene: SKScene {
         let highScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
         highScoreLabel.fontColor = UIColor.redColor()
         highScoreLabel.fontSize = 20
-        highScoreLabel.position = CGPoint(x: self.frame.width / 2, y: self.frame.height - 100.0)
+        highScoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.maxY * 8/9)
         highScoreLabel.text = "Levels unlocked: \(Level.gameLevel)"
         return highScoreLabel
     }
